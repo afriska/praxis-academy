89c865f816634552574867be1ed4c41dbe674ab4
papawpy <pawpaw.py@gmail.com>
2023-02-16 06:42:12 +0700

    backend

---
+++ backend/minggu-01/hari-03/tipeData.py
@@ -0,0 +1,29 @@
+#Data Types
+
+x = 5
+print(type(x))
+y = "Hello World"
+print(type(y))
+z = 1j
+print(type(z))
+a = 20
+print(type(a))
+b = 20.0
+print(type(b))
+c = True
+print(type(c))
+d = None
+d = 23
+print(type(d))
+e = ["apple", "banana", "cherry"]
+f = ("apple", "banana", "cherry")
+g = {"apple", "banana", "cherry"}
+h = {"name" : "John", "age" : 36}
+print(e)
+print(f)
+print(g)
+print(h)
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
